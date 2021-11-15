3b936ec573f80eec26a45867f2fb2f5bcfbaf643
Dev-v2 <89397215+New-Dev3@users.noreply.github.com>
2021-11-15 15:44:19 +0530

    Initial commit

--- Natsuki/modules/__shield.py
+++ Natsuki/modules/__shield.py
@@ -28,7 +28,7 @@ from telethon.tl.types import ChatBannedRights
 from Natsuki import BOT_ID
 from Natsuki.conf import get_int_key, get_str_key
 
-# from HexzyBot.db.mongo_helpers.nsfw_guard import add_chat, get_all_nsfw_chats, is_chat_in_db, rm_chat
+# from Natsuki.db.mongo_helpers.nsfw_guard import add_chat, get_all_nsfw_chats, is_chat_in_db, rm_chat
 from Natsuki.pyrogramee.telethonbasics import is_admin
 from Natsuki.events import register
 from Natsuki import MONGO_DB_URI 
